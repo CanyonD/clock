771d447e84e1638fe14bdcd44055ffeb417efdf7
Canyon Di <canyond8@gmail.com>
2019-10-07 22:09:27 +0300

    Updates

---
+++ precache-manifest.45aff3a351e4caa43de7b918b3137fd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e156c49a7ea245dabb99cc3dcb0fa65",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "2c9b72b0637f38510ba1",
+    "url": "/clock/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7302773daffdfcd31164",
+    "url": "/clock/static/js/2.b05bfde0.chunk.js"
+  },
+  {
+    "revision": "2c9b72b0637f38510ba1",
+    "url": "/clock/static/js/main.f8560908.chunk.js"
+  },
+  {
+    "revision": "1a0f36b324ed55e20fe4",
+    "url": "/clock/static/js/runtime-main.81151f43.js"
+  }
+]);
